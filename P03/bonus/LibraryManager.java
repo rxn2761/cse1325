5c83df11623c9960da44486fb43fea07989bac11
Rodney Nguyen <rxn2761@mavs.uta.edu>
2023-09-07 22:50:58 -0500

    P03 bonus

--- P03/bonus/LibraryManager.java
+++ P03/bonus/LibraryManager.java
@@ -36,21 +36,5 @@ public class LibraryManager
         L.checkOut(BookIndex, pa);
 
         L.toStringLibrary();
-
-
-
-        /*Publication P1 = new Publication("Fahrenheit 451", "Ray Bradbury", 2010);
-        Publication P2 = new Publication("To Kill a Mockingbird", "Harper Lee", 2016);
-        Publication P3 = new Publication("The Catcher in the Rye", "J.D. Salinger", 2013);
-
-        Library L = new Library("Arlington Public Library");
-        L.addPublication(P1);
-        L.addPublication(P2);
-        L.addPublication(P3);
-        Patron Patron1 = new Patron("Rod Nguyen", "rxn2761@mavs.uta.edu");
-
-        L.checkOut(1, Patron1);
-
-        L.toStringLibrary();*/
     }
 }
