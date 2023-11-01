4a846bec63bc3cba6ea637ec42adefb69e96d009
Rodney Nguyen <rxn2761@mavs.uta.edu>
2023-10-31 23:08:32 -0500

    P09 full_credit

---
+++ P09/full_credit/Shapes.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+using std::string;
+
+
+class Shape {
+public:
+    virtual string name() const {
+        return "";
+    }
+    virtual double area() const = 0;
+    string toString() {
+        return name() + " with area " + std::to_string(area());
+    }
+};
+
+class Triangle: public Shape {
+private:
+    double _height;
+    double _base;
+
+public:
+    Triangle(double height, double base) {
+        _height = height; _base = base;
+    }
+    string name() const override {
+        string returnString;
+        returnString = "(" + std::to_string(_height) + " X " +  std::to_string(_base) + ") / 2";
+        return returnString + " Triangle";
+    }
+    double area() const override {
+        double Area = (_height * _base) / 2;
+        return Area;
+    }
+};
+
+class Circle: public Shape {
+private:
+    double _radius;
+
+public:
+    Circle(double radius) {
+        _radius = radius;
+    }
+    string name() const override {
+        return "Circle of radius " + std::to_string(_radius);
+    }
+    double area() const override {
+        return M_PI * pow(_radius, 2);
+    }
+};
+
+int main() {
+    std::vector<Shape*> S;
+
+    Triangle triangle(4.0, 5.0);
+    Circle circle(6.0);
+    Triangle triangle2(2.56, 4.59);
+    Circle circle2(3.9);
+
+    S.push_back(&triangle);
+    S.push_back(&circle);
+    S.push_back(&triangle2);
+    S.push_back(&circle2);
+
+    for (int i = 0; i < S.size(); i++) {
+        std::cout << S[i]->toString() << std::endl;
+    }
+    return 0;
+}
